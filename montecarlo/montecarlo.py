032257fc9cacca3ce7584f283171a2159af7ba18
Eugene Fairley <ecfairley@ucdavis.edu>
2016-07-07 17:48:01 -0700

    github directory working

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -5,12 +5,12 @@ import numpy as np
 
 def main():
 	#Change these to change which files to simulate with
-	VaryInp('ATP_mc').vary()
-	VaryDat("bdia_mc0").vary()
-	VaryDat("Bstk_mc0").vary()
-	VaryDat("B_mc0").vary()
-	VaryDat("QOL_mc0",5,"   %6.4f").vary()
-	VaryCst("cstval").vary()
+	VaryInp('SBP1').vary()
+	VaryDat("Bdia").vary()
+	VaryDat("Bstk",lines=2).vary()
+	VaryDat("B",lines=2).vary()
+	VaryDat("qol",5,"   %6.4f").vary()
+	#VaryCst("cstval").vary()
 
 class Varier:
 	"""Vary data file according to distribution
@@ -22,30 +22,32 @@ class Varier:
 		format: Format string defining the way to output numbers to the output 
 		file
 	"""	
-	def __init__(self, fname, spaces, format):
-		self.f = open('modfile/' + fname + '.mc','w')
-		self.means = read_lines(fname + '.dat')
-		self.std = read_lines(fname + '.dat')
+	def __init__(self, fname, spaces, format, lines=1):
+		self.mc0 = open('modfile/' + fname + '_mc.dat','w')
+		self.save = open('modfile/' + 'MC' + fname + '.txt','a')
+		self.means = read_lines(fname + '_mc.dat')
+		self.std = read_lines(fname + '_mc.dat')
 		self.spaces = spaces
+		self.lines = lines
 		self.format = format
 
-	def _format_lst(self, _lst):
+	def _format_lst(self,f, _lst):
 		fstring = ' '*self.spaces + self.format*len(_lst)
-		print>>self.f, fstring % tuple(_lst)
+		print>>f, fstring % tuple(_lst)
 
 	def vary(self):
 		for mean_line,std_line in zip(self.means,self.std):
 			self._vary_line(mean_line,std_line)
-		self.f.close()
+		self.mc0.close()
 
 	def _vary_line(self,mean_line,std_line):
 		sp_mean_line = mean_line.split()
 		sp_std_line = std_line.split()
 		if is_num_line(sp_mean_line):
 			sp_out = self._dist(sp_mean_line,sp_std_line)
-			self._format_lst(sp_out)
+			self._format_lst(self.mc0,sp_out)
 		else: 
-			replace_line(self.f,mean_line)
+			replace_line(self.mc0,mean_line)
 
 	def _dist(self, *dist_args): pass
 
@@ -61,17 +63,25 @@ class VaryDat(Varier):
 		format: Format string defining the way to output numbers to the output 
 		file
 	"""	
-	def __init__(self, fname, spaces=11, format="   %9.6f"):
-		Varier.__init__(self,fname, spaces, format)
+
+	def __init__(self, fname, spaces=11, format="   %9.6f", lines=1):
+		Varier.__init__(self,fname, spaces, format, lines)
 		self.n_line = 0
 
 
 	def _dist(self, mean, std):
-		if self.n_line%6 == 0: 
+		if self.n_line%6 == 0:
 			self.rnd = np.random.randn(len(mean)-1)
-		self.n_line += 1
-		return [float(x[0])+self.rnd[i]*float(x[1]) for i,
+		rnd_out = [float(x[0])+self.rnd[i]*float(x[1]) for i,
 						x in enumerate(zip(mean[1:],std[1:]))]
+
+		if self.n_line/6 < self.lines and self.n_line%6 == 0:
+			self._format_lst(self.save,rnd_out)
+
+		self.n_line += 1
+		return rnd_out
+
+
 
 	
 class VaryCst(Varier):
@@ -80,25 +90,34 @@ class VaryCst(Varier):
 		
 	Creates cstval_mc.mc from cstval_mc0.dat. Specifically for cost files
 	
-	Args:
+	Attributes:
 		fname: A string representing the name of a .dat file
 		spaces: Integer number of spaces before the leftmost number
-		format: Format string defining the way to output numbers to the output file
+		format: Format string defining the way to output numbers to the output 
+		file
 	"""	
 
 	def __init__(self, fname, spaces=3, format="%-8.2f       "):
 		Varier.__init__(self,fname, spaces, format)
 
-	def _dist(self, mean, std):
+	def _dist(self, mean, st): 
 		rnd = np.random.randn()
-		return [max(float(x)+rnd*float(y),0) for x,y in zip(mean,std)]
+		return [max(float(x)+rnd*float(y),0) for x,y in zip(mean,st)]
 
 
 class VaryInp:
+	"""
+
+	Attributes:
+		means: list of lines in original file
+		std: list of lines in standard deviation file
+
+	"""
+
 	def __init__(self, fname):
-		self.f = open(fname + '.mc','w')
+		self.f = open(fname + '_mc.inp','w')
 		self.rd = read_lines(fname + '.sdind')
-		self. means = read_lines(fname + '.inp')
+		self.means = read_lines(fname + '_mc0.inp')
 		self.std = []
 		self._build_std()
 		
@@ -114,11 +133,12 @@ class VaryInp:
 
 	def _vary_line(self,line):
 		replace = 0
-		for tup in self.std:
+		rnd = np.random.randn(len(self.std))
+		for i,tup in enumerate(self.std):
 			if self._find_keyword(line,tup[0]):
 				if replace == 1:
 					print "error: keywords overlap"
-				out = float(line.split()[0]) + np.random.randn()*float(tup[1])
+				out = float(line.split()[0]) + rnd[i]*float(tup[1])
 				print>>self.f, out
 				replace = 1
 		if replace == 0:
