d74ad151081a6b1cb19d43de9af4b63b736077fe
Eugene Fairley <ecfairley@ucdavis.edu>
2016-07-09 20:41:35 -0700

    changed to object oriented design -many changes

--- format.py
+++ format.py
@@ -3,163 +3,175 @@ import sys
 import os.path
 import re
 
-#asbasdas
-def print_lines(n,y,data,outfile,base_year,categories):
-	"""Add one section of numbers to .frmt file.
+def main():
+	if len(sys.argv) < 2:
+		print("Input file name: e.g. 'format.py filename'")
+		sys.exit()
 
-	Args:
-		n: integer representing line in data file
-		y: integer representing current year - base_year
-		data: list of (string) lines in .out file
-		outfile: .frmt file to write to
-		base_year: integer first year simulation is run on
-		categories: integer number of categories to consider
+	if not os.path.isfile(sys.argv[1] + ".out"):
+		print("Invalid File Name")
+		print("Execute by typing 'format.py filename'")
+		sys.exit()
+
+	reform = Reformatter()
+	reform.format("NEW CHD CASES",2)
+	reform.format("CVD PREVALENCE - first of year",1,["Prevalence"])
+	reform.format("NEW INTERVENED CHD CASES",2)
+	reform.format("NUMBER TREATED",2,["DE","DH"])
+	reform.format("CHD DEATHS",2)
+	reform.format("CHD Deaths (Bridge)",3)
+	reform.format("Acute CHD Death 11-17",3)
+	reform.format("Acute CHD Death 1-10",3)
+	reform.format("Acute CHD Deaths (Bridge & DH)",3,["Arrest","MI","Revasc"])
+	reform.format("Acute CHD Deaths (Bridge)",3,
+					["MI","Arrest In-Hospital","Arrest Pre-Hospital"])
+	reform.format("Chronic CHD Death",3)
+	reform.format("Non-CVD Death",2)
+	reform.format("Non-CVD Death (DE)",3)
+	reform.format("DE Intervened Non-CVD Deaths", 3)
+	reform.format("Non-CVD Death (DH)",3)
+	reform.format("Total Pop (DE)",1)
+	reform.format("Total Pop (DH 11-17)",1)
+	reform.format("Total Pop (DH 1-10)",1)
+	reform.format("Total DE Diabetes Pop",1)
+	reform.format("NEW DIABETES CASES",2)
+	reform.format("CVD EVENTS", 4, ["ANGINA", "ARREST", "MI", "STROKE"],17)
+	reform.format("Revascularization Events",4,
+					["AMI CABG","AMI PTCA","ISO CABG","ISO PTCA"],)
+	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+					["ANGINA","REVSC","MI","TBD"])
+	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+					["REVSC + MI","TBD","TBD","TBD"],15)
+	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+					["TOT/ISC","MT+STROKE","BRIDGE MI","BRIDGE MI + ARR"],26)
+	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+					["BRIDGE ARREST","BRIDGE ANGINA","BRIDGE MI + REVSC","BRIDGE TOT STROKE"],37)
+	reform.format("CVD POPULATION DISTRIBUTION BY STATE",1,["BRIDGE HEM STROKE"],48)
+	reform.format("Total MI",1)
+	reform.format("Bridge MI",1)
+	reform.format("DH First MI (1,2,14)",1)
+	reform.format("First MI",1)
+	reform.format("Total MI",1)
+	reform.format("DH 2nd MI (11,12,15, and dhxevt)",1)
+	reform.format("DH Re-MI (3-8,11-13,15)",1)
+	reform.format("DH MI Stroke (9,10,16,17)",1)
+	reform.format("TOTAL STROKE",2)
+	reform.format("BRIDGE STROKE",2)
+	reform.format("NEW INTERVENED STROKE CASES",2)
+	reform.format("First Stroke (DH ONLY)",2)
+	reform.format("STROKE DEATHS",2)
+	reform.format("TOTAL DEATHS",2)
+	reform.format("NON-CVD Costs",1,["BACKGROUND HEALTHCARE COST"],5)
+	reform.format("TOTAL CHD COSTS",1,["Age/Sex Breakdown($)"],12)
+	reform.format("TOTAL PREVENTION COSTS",1,["Age/Sex Breakdown($)"],6)
+	reform.format("TOTAL STROKE COSTS",1,["Age/Sex Breakdown($)"],5)
+	reform.format("Total QALY",1,["Age/Sex Breakdown"],6)
+	reform.format("Total DH QALY",1,["Age/Sex Breakdown"],6)
+
+
+class Reformatter(object):
+	"""Used to build formatted .frmt file from .out file
+
 	"""
 
-	frmt_data = []
-	#get correct data/lines and remove extra characters
-	for i in range(n,n+6):
-		data[i] = data[i].replace('. ', '') 
-		data[i] = data[i].replace('.\n','')
-		#this is for CVD prevalence -- don't want 'x/y' just want rate
-		data[i] = re.sub(r'[0-9]*./ \s*[0-9]*','',data[i])
-		frmt_data += [s for s in data[i].split()]
-	outfile.write(str(base_year + y))
-	line = []
-	#get data in correct order
-	for i in range(categories*2):
-		for j in range(6):
-			line.append(frmt_data[i + (categories*2 + 1)*j + 1])
-	into = '{:>12} ' + '{:>15} '*(12*categories-1) 
-	outfile.write(into.format(*line) + "\n")
+	def __init__(self):
+		self.topline = ['M35-44', 'M45-54', 'M55-64', 'M65-74', 'M75-84', 'M85-94', 
+			'F35-44',  'F45-54',  'F55-64',   'F65-74',  'F75-84',  'F85-94']
+		self.outfile = open(sys.argv[1] + '.frmt','w')
+		self.lines_list = self.get_lines()
+		self.base_year = int(self.lines_list[9])
+		self.topline_format = lambda categories: 'Year{:>12} ' + '{:>15} '*(12*categories-1)
+		self.num_format = lambda categories: '{}{:>12} ' + '{:>15} '*(12*categories-1)
+		self.category_format = lambda categories: '    {:>12}' + '{:>192}'*(categories-1)
+		self.max_lines_after = 15
 
+	def get_lines(self):
+		with open(sys.argv[1] + ".out", 'r') as myfile:
+			return myfile.readlines()
 
-def setup(title,categories):
-	"""Wrapper for creating .frmt file.
-
-	Used for default formats -- two categories with
-	Age/Sex and CHD incedence rate or one category
-	which is Age/Sex Breakdown
-
-	Args:
-		title: label to look for e.g. "NEW CHD CASES"
-		categories: integer number of categories to look for
-	"""
-
-	outfile.write(title + '\n')
-	outfile.write('Year')
-	into = '{:>12} ' + '{:>15} '*(12*categories-1)
-	topline_f = []
-	for i in range(categories):
-		topline_f += topline
-	outfile.write(into.format(*topline_f) + "\n")
-	with open(sys.argv[1] + ".out", 'r') as myfile:
-		data = myfile.readlines()
-	i = 0
-	years = 0
-	base_year = int(data[9])
-	for line in data:
-		#find CORRECT title (e.g. not title + ' rate %' or 'Acute ' + title)
-		if (line.find(title + "     ") != -1 or 
+	def find_title(self, line, title):
+		return (line.find(title + "     ") != -1 or 
 			line.find(title + "\n") != -1 and 
-			line.find("Acute " + title) == -1): 
-			#sections with >1 category have 7 lines before the numbers
-			if categories != 1:  
-				print_lines(i+7, years,data,outfile,base_year,categories)
-			#sections with 1 category have 6 lines before numbers
-			else:
-				print_lines(i+6, years,data,outfile,base_year,categories)
-			years += 1
-		i+=1
-	outfile.write("\n")
+			line.find("Acute " + title) == -1)
+
+	def full_topline(self, categories):
+		topline_full = []
+		for i in range(categories):
+			topline_full += self.topline
+		return topline_full
+
+	def write_topline(self, title, categories, ctg_list):
+		print>>self.outfile, title
+		if ctg_list:
+			full_cat_format = self.category_format(categories)
+			print>>self.outfile, full_cat_format.format(*ctg_list)
+
+		topline_full = self.full_topline(categories)
+		full_top_format = self.topline_format(categories)
+		print>>self.outfile, full_top_format.format(*topline_full)
+
+	def format(self, title, categories, ctg_list=[], linesdown=0):
+		""" Reformats sections with particular label
+
+		Args:
+			title: String to look for to reformat
+			ctg_list: List of categories to search for (e.g. ANGINA, ARREST, MI)
+			categories: Integer number of categories
+			linesdown: Integer number of lines below the title to search for block
+			(only used if it doesn't make sense to find next line starting with numbers)
+		"""
+		
+		self.write_topline(title,categories,ctg_list)
+		self.format_blocks(categories,linesdown,title)  
+
+	def format_blocks(self, categories, linesdown, title):
+		cur_year = 0
+		for line_num,line in enumerate(self.lines_list):
+			if self.find_title(line,title):
+				self.format_block(line_num,cur_year,categories, linesdown)
+				cur_year+=1
+		print>>self.outfile, "\n",	
+
+	def format_block(self, line_num, cur_year, categories, linesdown):
+		start_line = linesdown + line_num if linesdown!=0 else self.next_num_line(line_num)
+		num_list = self.get_number_list(start_line,categories)
+		self.format_num_line(categories,num_list,cur_year)
+
+	def format_num_line(self, categories,num_list,cur_year):
+		full_format = self.num_format(categories)
+		print>>self.outfile, full_format.format(self.base_year + cur_year,*num_list)
+
+	def get_number_list(self, start_line, categories):
+		num_list = []
+		self.replace_bad_chars(start_line)
+		for i in range(6):
+			num_list += [num for num in self.lines_list[i + start_line].split()]
+		return self.reorder_list(num_list,categories)
+
+	def reorder_list(self, num_list, categories):
+		reordered = []
+		for i in range(categories*2):
+			for j in range(6):
+				reordered.append(num_list[i + (categories*2 + 1)*j + 1])
+		return reordered
+
+	def replace_bad_chars(self, start_line):
+		for i in range(start_line, start_line + 6):
+			self.lines_list[i] = self.lines_list[i].replace('. ', '') 
+			self.lines_list[i] = self.lines_list[i].replace('.\n','')
+			#this is for CVD prevalence -- don't want 'x/y' just want rate
+			self.lines_list[i] = re.sub(r'[0-9]*./ \s*[0-9]*','',
+												self.lines_list[i])
+
+	def next_num_line(self, line_num):
+		for i in range(self.max_lines_after):
+			if len(self.lines_list[i + line_num].split()) == 0:
+				continue
+			first_num = self.lines_list[i + line_num].split()[0][0]
+			if str.isdigit(first_num):
+				return i + line_num
+		return -1
 
 
-def setup2(title,categories,ctg_list,linesdown):
-	"""Wrapper for creating .frmt file.
-
-	Args:
-		title: label to look for e.g. "NEW CHD CASES"
-		categories: integer number of categories to look for
-		ctg_list: list of string titles of categories (len(ctg_list)=categories)
-		linesdown: integer number of lines below title to look for numbers
-	"""
-	outfile.write(title + '\n')
-	topline_f = []
-	into = '{:>12} ' + '{:>15} '*(12*categories-1)
-	for i in range(categories):
-		topline_f += topline
-	line2 = '    {:>12}' + '{:>192}'*(categories-1)
-	outfile.write(line2.format(*ctg_list) + "\n")
-	outfile.write('Year')
-	outfile.write(into.format(*topline_f) + "\n")
-	with open(sys.argv[1] + ".out", 'r') as myfile:
-		data = myfile.readlines()
-	i = 0
-	years = 0
-	base_year = int(data[9])
-	for line in data:
-		#if we find CORRECT title (e.g. not title + ' rate %')
-		if (line.find(title + "     ") != -1 or 
-			line.find(title + "\n") != -1):
-			print_lines(i+linesdown, years,data,outfile,base_year,categories)
-			years += 1
-		i+=1
-	outfile.write("\n")
-
-
-topline = ['M35-44',   'M45-54',   'M55-64',   'M65-74',   'M75-84',   'M85-94',   
-			'F35-44',   'F45-54',   'F55-64',   'F65-74',   'F75-84',   'F85-94']
-
-if not os.path.isfile(sys.argv[1] + ".out"):
-	print("Invalid File Name")
-	sys.exit()
-fname = sys.argv[1] + ".frmt"
-outfile = open(fname,'w')
-
-setup("NEW CHD CASES",2)
-setup2("CVD PREVALENCE - first of year",1,["Prevalence"],2)
-setup("NEW INTERVENED CHD CASES",2)
-setup2("NUMBER TREATED",2,["DE","DH"],4)
-setup("CHD DEATHS",2)
-setup("CHD Deaths (Bridge)",3)
-setup("Acute CHD Death 11-17",3)
-setup("Acute CHD Death 1-10",3)
-setup2("Acute CHD Deaths (Bridge & DH)",3,["Arrest","MI","Revasc"],4)
-setup2("Acute CHD Deaths (Bridge)",3,["MI","Arrest In-Hospital","Arrest Pre-Hospital"],4)
-setup("Chronic CHD Death",3)
-setup("Non-CVD Death",2)
-setup("Non-CVD Death (DE)",3)
-setup("DE Intervened Non-CVD Deaths", 3)
-setup("Non-CVD Death (DH)",3)
-setup("Total Pop (DE)",1)
-setup("Total Pop (DH 11-17)",1)
-setup("Total Pop (DH 1-10)",1)
-setup("Total DE Diabetes Pop",1)
-setup("NEW DIABETES CASES",2)
-setup2("CVD EVENTS", 4, ["ANGINA", "ARREST", "MI", "STROKE"],17)
-setup2("Revascularization Events",4,["AMI CABG","AMI PTCA","ISO CABG","ISO PTCA"],4)
-setup2("CVD POPULATION DISTRIBUTION BY STATE",4,["ANGINA","REVSC","MI","TBD"],4)
-setup2("CVD POPULATION DISTRIBUTION BY STATE",4,["REVSC + MI","TBD","TBD","TBD"],15)
-setup2("CVD POPULATION DISTRIBUTION BY STATE",4,["TOT/ISC","MT+STROKE","BRIDGE MI","BRIDGE MI + ARR"],26)
-setup2("CVD POPULATION DISTRIBUTION BY STATE",4,["BRIDGE ARREST","BRIDGE ANGINA","BRIDGE MI + REVSC","BRIDGE TOT STROKE"],37)
-setup2("CVD POPULATION DISTRIBUTION BY STATE",1,["BRIDGE HEM STROKE"],48)
-setup("Total MI",1)
-setup("Bridge MI",1)
-setup("DH First MI (1,2,14)",1)
-setup("First MI",1)
-setup("Total MI",1)
-setup("DH 2nd MI (11,12,15, and dhxevt)",1)
-setup("DH Re-MI (3-8,11-13,15)",1)
-setup("DH MI Stroke (9,10,16,17)",1)
-setup("TOTAL STROKE",2)
-setup("BRIDGE STROKE",2)
-setup("NEW INTERVENED STROKE CASES",2)
-setup("First Stroke (DH ONLY)",2)
-setup("STROKE DEATHS",2)
-setup("TOTAL DEATHS",2)
-setup2("NON-CVD Costs",1,["BACKGROUND HEALTHCARE COST"],5)
-setup2("TOTAL CHD COSTS",1,["Age/Sex Breakdown($)"],12)
-setup2("TOTAL PREVENTION COSTS",1,["Age/Sex Breakdown($)"],6)
-setup2("TOTAL STROKE COSTS",1,["Age/Sex Breakdown($)"],5)
-setup2("Total QALY",1,["Age/Sex Breakdown"],6)
-setup2("Total DH QALY",1,["Age/Sex Breakdown"],6)
+if __name__ == '__main__':
+    main()
