8e85c54c52553d1ed75a2768c95a0c5621bfdbbb
Eugene Fairley <ecfairley@ucdavis.edu>
2016-07-27 10:24:17 -0700

    global macros, comments

--- format.py
+++ format.py
@@ -5,88 +5,101 @@ import sys
 import os.path
 import re
 
+#line of .outfile to look for base year
+BASE_YEAR_LINE = 9 
+
+#max lines after a title to look for 
+#a block of values (excluding when specified)
+MAX_LINES_AFTER = 15 
 
 def main():
 	checkInput()
 	
-	reform = Reformatter(sys.argv[1] + '.frmt')
+	output_extension = '.frmt'
+	filename = sys.argv[1]
+	reformatter = Reformatter(filename,output_extension)
 	
-	reform.format("NEW CHD CASES",2)
-	reform.format("CVD PREVALENCE - first of year",1,["Prevalence"])
-	reform.format("NEW INTERVENED CHD CASES",2)
-	reform.format("NUMBER TREATED",2,["DE","DH"])
-	reform.format("CHD DEATHS",2)
-	reform.format("CHD Deaths (Bridge)",3)
-	reform.format("Acute CHD Death 11-17",3)
-	reform.format("Acute CHD Death 1-10",3)
-	reform.format("Acute CHD Deaths (Bridge & DH)",3,["Arrest","MI","Revasc"])
-	reform.format("Acute CHD Deaths (Bridge)",3,
+	reformatter.format("NEW CHD CASES",2)
+	reformatter.format("CVD PREVALENCE - first of year",1,["Prevalence"])
+	reformatter.format("NEW INTERVENED CHD CASES",2)
+	reformatter.format("NUMBER TREATED",2,["DE","DH"])
+	reformatter.format("CHD DEATHS",2)
+	reformatter.format("CHD Deaths (Bridge)",3)
+	reformatter.format("Acute CHD Death 11-17",3)
+	reformatter.format("Acute CHD Death 1-10",3)
+	reformatter.format("Acute CHD Deaths (Bridge & DH)",3,["Arrest","MI","Revasc"])
+	reformatter.format("Acute CHD Deaths (Bridge)",3,
 					["MI","Arrest In-Hospital","Arrest Pre-Hospital"])
-	reform.format("Chronic CHD Death",3)
-	reform.format("Non-CVD Death",2)
-	reform.format("Non-CVD Death (DE)",3)
-	reform.format("DE Intervened Non-CVD Deaths", 3)
-	reform.format("Non-CVD Death (DH)",3)
-	reform.format("Total Pop (DE)",1)
-	reform.format("Total Pop (DH 11-17)",1)
-	reform.format("Total Pop (DH 1-10)",1)
-	reform.format("Total DE Diabetes Pop",1)
-	reform.format("NEW DIABETES CASES",2)
-	reform.format("CVD EVENTS", 4, ["ANGINA", "ARREST", "MI", "STROKE"],17)
-	reform.format("Revascularization Events",4,
+	reformatter.format("Chronic CHD Death",3)
+	reformatter.format("Non-CVD Death",2)
+	reformatter.format("Non-CVD Death (DE)",3)
+	reformatter.format("DE Intervened Non-CVD Deaths", 3)
+	reformatter.format("Non-CVD Death (DH)",3)
+	reformatter.format("Total Pop (DE)",1)
+	reformatter.format("Total Pop (DH 11-17)",1)
+	reformatter.format("Total Pop (DH 1-10)",1)
+	reformatter.format("Total DE Diabetes Pop",1)
+	reformatter.format("NEW DIABETES CASES",2)
+	reformatter.format("CVD EVENTS", 4, ["ANGINA", "ARREST", "MI", "STROKE"],17)
+	reformatter.format("Revascularization Events",4,
 					["AMI CABG","AMI PTCA","ISO CABG","ISO PTCA"],)
-	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+	reformatter.format("CVD POPULATION DISTRIBUTION BY STATE",4,
 					["ANGINA","REVSC","MI","TBD"])
-	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+	reformatter.format("CVD POPULATION DISTRIBUTION BY STATE",4,
 					["REVSC + MI","TBD","TBD","TBD"],15)
-	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+	reformatter.format("CVD POPULATION DISTRIBUTION BY STATE",4,
 					["TOT/ISC","MT+STROKE","BRIDGE MI","BRIDGE MI + ARR"],26)
-	reform.format("CVD POPULATION DISTRIBUTION BY STATE",4,
+	reformatter.format("CVD POPULATION DISTRIBUTION BY STATE",4,
 					["BRIDGE ARREST","BRIDGE ANGINA","BRIDGE MI + REVSC","BRIDGE TOT STROKE"],37)
-	reform.format("CVD POPULATION DISTRIBUTION BY STATE",1,["BRIDGE HEM STROKE"],48)
-	reform.format("Total MI",1)
-	reform.format("Bridge MI",1)
-	reform.format("DH First MI (1,2,14)",1)
-	reform.format("First MI",1)
-	reform.format("Total MI",1)
-	reform.format("DH 2nd MI (11,12,15, and dhxevt)",1)
-	reform.format("DH Re-MI (3-8,11-13,15)",1)
-	reform.format("DH MI Stroke (9,10,16,17)",1)
-	reform.format("TOTAL STROKE",2)
-	reform.format("BRIDGE STROKE",2)
-	reform.format("NEW INTERVENED STROKE CASES",2)
-	reform.format("First Stroke (DH ONLY)",2)
-	reform.format("STROKE DEATHS",2)
-	reform.format("TOTAL DEATHS",2)
-	reform.format("NON-CVD Costs",1,["BACKGROUND HEALTHCARE COST"],5)
-	reform.format("TOTAL CHD COSTS",1,["Age/Sex Breakdown($)"],12)
-	reform.format("TOTAL PREVENTION COSTS",1,["Age/Sex Breakdown($)"],6)
-	reform.format("TOTAL STROKE COSTS",1,["Age/Sex Breakdown($)"],5)
-	reform.format("Total QALY",1,["Age/Sex Breakdown"],6)
-	reform.format("Total DH QALY",1,["Age/Sex Breakdown"],6)
+	reformatter.format("CVD POPULATION DISTRIBUTION BY STATE",1,["BRIDGE HEM STROKE"],48)
+	reformatter.format("Total MI",1)
+	reformatter.format("Bridge MI",1)
+	reformatter.format("DH First MI (1,2,14)",1)
+	reformatter.format("First MI",1)
+	reformatter.format("Total MI",1)
+	reformatter.format("DH 2nd MI (11,12,15, and dhxevt)",1)
+	reformatter.format("DH Re-MI (3-8,11-13,15)",1)
+	reformatter.format("DH MI Stroke (9,10,16,17)",1)
+	reformatter.format("TOTAL STROKE",2)
+	reformatter.format("BRIDGE STROKE",2)
+	reformatter.format("NEW INTERVENED STROKE CASES",2)
+	reformatter.format("First Stroke (DH ONLY)",2)
+	reformatter.format("STROKE DEATHS",2)
+	reformatter.format("TOTAL DEATHS",2)
+	reformatter.format("NON-CVD Costs",1,["BACKGROUND HEALTHCARE COST"],5)
+	reformatter.format("TOTAL CHD COSTS",1,["Age/Sex Breakdown($)"],12)
+	reformatter.format("TOTAL PREVENTION COSTS",1,["Age/Sex Breakdown($)"],6)
+	reformatter.format("TOTAL STROKE COSTS",1,["Age/Sex Breakdown($)"],5)
+	reformatter.format("Total QALY",1,["Age/Sex Breakdown"],6)
+	reformatter.format("Total DH QALY",1,["Age/Sex Breakdown"],6)
 
 
 def checkInput():
 	if len(sys.argv) < 2:
-		print("Input file name: e.g. 'format.py filename'")
+		print("Input file name: e.g. 'format.py [filename]'")
 		sys.exit()
 
 	if not os.path.isfile(sys.argv[1] + ".out"):
 		print("Invalid File Name")
-		print("Execute by typing 'format.py filename'")
+		print("Execute by typing 'format.py [filename]'")
 		sys.exit()
 
 
 class Reformatter(object):
 	"""Used to build formatted file from .out file
 
+	Attr:
+		topline: list of headers within categories
+		formatted_file: file to be written to
+		outfile: CVDOutfile object containing information for .out file
+		*_format: format string based on number of total categories
 	"""
 
-	def __init__(self,outfile_name):
+	def __init__(self,filename,ext):
 		self.topline = ['M35-44', 'M45-54', 'M55-64', 'M65-74', 'M75-84', 'M85-94', 
 			'F35-44',  'F45-54',  'F55-64',   'F65-74',  'F75-84',  'F85-94']
-		self.formatted_file = open(outfile_name,'w')
-		self.outfile = CVDOutfile(outfile_name)
+		self.formatted_file = open(filename + ext,'w')
+		self.outfile = CVDOutfile(filename)
 		self.topline_format = lambda categories: 'Year{:>12} ' + '{:>15} '*(12*categories-1)
 		self.num_format = lambda categories: '{}{:>12} ' + '{:>15} '*(12*categories-1)
 		self.category_format = lambda categories: '    {:>12}' + '{:>192}'*(categories-1)
@@ -144,15 +157,21 @@ class Reformatter(object):
 
 
 class CVDOutfile:
+	"""Contains information of .out file
+
+	Attr:
+		lines_list: list of lines in .out file
+		base_year: integer first year of simulation
+		num_lines: integer number of lines in lines_list
+	"""
 
 	def __init__(self,filename):
-		self.lines_list = self._get_lines()
-		self.base_year = int(self.lines_list[9])
-		self.max_lines_after = 15
+		self.lines_list = self._get_lines(filename)
+		self.base_year = int(self.lines_list[BASE_YEAR_LINE])
 		self.num_lines = len(self.lines_list)
 
-	def _get_lines(self):
-		with open(sys.argv[1] + ".out", 'r') as myfile:
+	def _get_lines(self,filename):
+		with open(filename + ".out", 'r') as myfile:
 			return myfile.readlines()
 
 	def find_title(self, line_num, title):
@@ -161,7 +180,7 @@ class CVDOutfile:
 			self.lines_list[line_num].find("Acute " + title) == -1)
 
 	def _replace_bad_chars(self, start_line):
-		#Fix section of numbers
+		"""Fix section of numbers"""
 		for i in range(start_line, start_line + 6):
 			self.lines_list[i] = self.lines_list[i].replace('. ', '') 
 			self.lines_list[i] = self.lines_list[i].replace('.\n','')
@@ -170,7 +189,7 @@ class CVDOutfile:
 												self.lines_list[i])
  
 	def next_num_line(self, line_num):
-		for i in range(self.max_lines_after):
+		for i in range(MAX_LINES_AFTER):
 			line_length = len(self.lines_list[i + line_num].split())
 			if line_length > 0:
 				first_char = self.lines_list[i + line_num].split()[0][0]
@@ -188,12 +207,18 @@ class CVDOutfile:
 
 
 class NumBlock:
+	"""Block of values for one year in .out file
+	
+	Attr:
+		num_list: list of the values
+	"""
 	def __init__(self,lines,categories):
 		self.num_list=[]
 		self._parse_block(lines)
 		self._reorder_block(self.num_list,categories)
 
 	def _reorder_block(self, num_list, categories):
+		"""Puts block in desired order for printing"""
 		reordered = []
 		for i in range(categories*2):
 			for j in range(6):
