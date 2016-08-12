6814ad57b890266ade5aefa98aae50439ace16bf
Eugene Fairley <ecfairley@ucdavis.edu>
2016-08-12 11:55:36 -0700

    update mc.bat for -z flag, new files

--- mc.bat
+++ mc.bat
@@ -1,18 +1,23 @@
 @echo off
 @echo %time%
-Del modfile\MC*.txt
+Del modfile\MC*.txt 
+mkdir output_files 
+mkdir sims 
 
 set files=base,1000,1039,10100
 set /a iterations=1000
 
 for /L %%A in (0,1,%iterations%) do (
-	sim_beta.py %%A%
-	sim_inputs.py %files% %%A%
-	for %%f in (%files%) do ( 
+	if  "%%A"=="0" (
+		py montecarlo.py %files% -z
+	) else (
+		py montecarlo.py %files%
+	)
+	for %%f in (%files%) do (
+		Del *.out
 	  	CHDMOD90<%%f_mc.inp>junk.txt
-	  	SSB.py %%f
-	  	copy %%f.ssb output_ssb\%%f_%%A%.ssb >NUL
+	  	py format.py %%f
+	  	copy %%f.frmt output_files\%%f_%%A%.frmt >NUL
 	  	copy outfile.dat sims\%%f_%%A%.dat >NUL
-	  	Del %%f.out
 	)
 )
